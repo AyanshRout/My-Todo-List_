566bed0b8ea87c07ff5a055f384b5d3e7d66aa52
AyanshRout <125910491+AyanshRout@users.noreply.github.com>
2023-12-28 13:49:37 -0800

    Update todo-list.js

--- todo-list.js
+++ todo-list.js
@@ -1,3 +1,24 @@
+const createCursor = (x  , y) =>{
+    const cursor = document.createElement('div')
+    cursor.className = 'cursor'
+    cursor.style.left = `${x}px`
+    cursor.style.top = `${y}px`
+    return cursor
+}
+const removeCursorFromDom = cursor =>{
+    const timeout = setTimeout(()=> {
+        cursor.remove()
+        clearTimeout(timeout)
+
+    },1000)
+
+}
+window.addEventListener('click', event =>{
+    const cursor = createCursor(event.pageX, event.pageY)
+    document.body.append(cursor)
+    removeCursorFromDom(cursor)
+})
+
 let todos
   const savedTodos = JSON.parse(localStorage.getItem('todos'))
 
